0557ebd0565ae9df49621b980e3b13b3e4b03aa1
my pc <wussne@gmail.com>
2025-11-28 00:33:58 +0700

    added some types + test commit

--- public/src/components/VideoPlayer.tsx
+++ public/src/components/VideoPlayer.tsx
@@ -1,20 +1,12 @@
 import { useState } from 'react';
 
-function VideoChoose(/* тут пропсы для setState и сами state  */) {
-    /* тут функция выбора фильмов если source пустой, окно заменяющее проигрыватель на время пока source не выбран */
-}
-
-function VideoPlayer(/* сюда пропсы для setState и сами state(в частности source), дополнительно в этой функции будет работа с websocket для синхронизации для совместного просмотра */) {
-    /* тут видео плеер проигрывающий видео */
-}
-
 export default function VideoRoute() {
     const [source, setSource] = useState<string | null>(null);
 
   return (
     <div className='border w-full h-full rounded-xl'>
 
-        {source ? 'yes' : 'no'}
+        <video src="https://dlcache4.vibio.tv/e40ff9657f0fe5e44dcd56f464ed8aaa/3105/3105757/output.lq.mp4" className='w-full h-full'></video>
 
     </div>
   );
--- public/src/types/video.types.ts
+++ public/src/types/video.types.ts
@@ -1,4 +1,8 @@
-export interface VideoState {
-    source: string,
-    
+import { User } from './user.types'
+
+export default interface VideoState {
+    source: 'yt' | 'vk'  | 'zona' | 'url', // types of sources( youtube, vk, zona, only url(for test) )
+    url: null | string, // url must be null(if we have video id from vk or youtube) or string(if we have zona or url source)
+    isPaused: boolean, // state for pausing 
+    inRoom: object,
 }
